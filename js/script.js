481c062e0fefc44e810920eca4d1dc5aebe99805
dvankevich <dvankevich@gmail.com>
2024-08-24 22:25:42 +0300

    add convert script

--- js/script.js
+++ js/script.js
@@ -12,3 +12,21 @@ function isRunningOnLocalhost() {
 if (isRunningOnLocalhost()) {
   console.log('web page running in localhost');
 }
+
+// from https://codepen.io/jlengstorf/pen/rNMpJNy
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const data = new FormData(event.target);
+
+  const formJSON = Object.fromEntries(data.entries());
+
+  // for multi-selects, we need special handling
+  //formJSON.snacks = data.getAll('snacks');
+
+  const results = document.querySelector('.results pre');
+  results.innerText = JSON.stringify(formJSON, null, 2);
+}
+
+const form = document.querySelector('.question-form');
+form.addEventListener('submit', handleFormSubmit);
